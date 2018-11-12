2e0c10f8aa559c2ecde2a6cf868e9fc431228d9c
Thanh Tung <vuthanhtunghayquen98@gmail.com>
2018-11-13 00:17:41 +0700

    hoan thien lan 1

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -1,26 +1,34 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
+import javafx.stage.*;
 
-
+import java.io.*;
+import java.net.Socket;
 
 public class Client extends Application {
-    Stage stage;
+    private Stage stage;
     private double sceneWidth = 750;
     private double sceneHeight = 516;
-    Scene sceneLogin,sceneCentre,scenePlay;
+    private Scene sceneLogin,scenePlay;
+    private ObjectInputStream reader;
+    private ObjectOutputStream writer;
+    private TextField tfUsername;
+    private int id,numberOfClickedButton;
+    private boolean isPlaying;
+    private int[][] table;
+    private GridPane paneTable;
+    private Button[][] buttons;
+    private ImageView imageView;
+    private TextArea chatMessages,message;
     public static void main(String[] args) {
         launch(args);
     }
@@ -28,7 +36,6 @@ public class Client extends Application {
     public void start(Stage primaryStage){
         stage = primaryStage;
         login();
-
     }
     public void login(){
         Label gameName = new Label("Game Caro");
@@ -40,7 +47,7 @@ public class Client extends Application {
 
         Label lbUsername = new Label("Username");
         lbUsername.setPrefWidth(80);
-        TextField tfUsername = new TextField();
+        tfUsername = new TextField();
         tfUsername.setPromptText("Enter your username here!");
         tfUsername.setPrefColumnCount(20);
 
@@ -73,47 +80,56 @@ public class Client extends Application {
         stage.setScene(sceneLogin);
         stage.show();
     }
-    public void centre(){
+    public void play(){
+        table = new int[10][10];
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++) table[i][j] = -1;
+        }
+        isPlaying = false;
+        buttons = new Button[10][10];
+        numberOfClickedButton = 0;
+
+        buildGUI();
+        getConnection();
 
     }
-    public void play(){
-        GridPane paneTable = new GridPane();
+    public void buildGUI(){
+        paneTable = new GridPane();
         paneTable.setAlignment(Pos.TOP_LEFT);
         for(int i=0;i<10;i++){
             for(int j=0;j<10;j++){
-                Button btn = new Button();
+                buttons[i][j] = new Button();
                 int row = i, col = j;
-                btn.setPrefSize(40,40);
-                paneTable.add(btn,i,j);
+                buttons[i][j].setPrefSize(40,40);
+                buttons[i][j].setOnAction(e -> choose(row,col,buttons[row][col]));
+                paneTable.add(buttons[i][j],j,i);
             }
         }
 
         VBox paneUser = new VBox(20);
-
         Label username = new Label();
-        username.setText("Player one");
+        username.setText(tfUsername.getText());
         username.setFont(new Font(20));
 
-        TextArea chatMessages = new TextArea();
+        chatMessages = new TextArea();
         chatMessages.setPrefColumnCount(15);
         chatMessages.setPrefHeight(90);
         chatMessages.setEditable(false);
 
-        ImageView imageView = new ImageView();
-        imageView.setImage(new Image("baobinh.jpg"));
+        imageView = new ImageView();
         imageView.setFitHeight(180);
         imageView.setFitWidth(280);
 
         HBox paneChat = new HBox(5);
 
-        TextArea message = new TextArea();
+        message = new TextArea();
         message.setWrapText(true);
         message.setPrefColumnCount(15);
         message.setPrefHeight(30);
 
         Button btnSend = new Button("SEND");
         btnSend.setFont(new Font(15));
-
+        btnSend.setOnAction(e -> sendMessage());
 
         paneChat.getChildren().addAll(message,btnSend);
         paneUser.getChildren().addAll(username,imageView,chatMessages,paneChat);
@@ -134,5 +150,197 @@ public class Client extends Application {
 
         stage.setScene(scenePlay);
         stage.show();
+    }
+    public void getConnection(){
+        try {
+            Socket socket = new Socket("127.0.0.1",5008);
+            reader = new ObjectInputStream(socket.getInputStream());
+            writer = new ObjectOutputStream(socket.getOutputStream());
+            Thread t = new Thread(new InformationReader());
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private class InformationReader implements Runnable{
+        @Override
+        public void run() {
+            Object object;
+            try{
+                while((object = reader.readObject()) != null){
+                    if(object instanceof Integer){
+                        id = (Integer) object;
+                        Platform.runLater(() -> setImage() );
+                        isPlaying = true;
+                    }
+                    if(object instanceof Cell){
+                        Cell cell = (Cell) object;
+                        int row = cell.getRow();
+                        int col = cell.getColumn();
+                        if(numberOfClickedButton % 2 == id) table[row][col] = id;
+                        else table[row][col] = (id+1)%2;
+                        numberOfClickedButton++;
+                        System.out.println(row + " " +  col);
+                        Platform.runLater(() -> updateCellPicture(row,col));
+                        if(numberOfClickedButton > 8){
+                            if(checkWin() == 1) win();
+                            if(checkWin() == 0) loose();
+                        }
+                    }
+                    if(object instanceof String){
+                        String text = (String) object;
+                        Platform.runLater(() -> chatMessages.appendText(text + "\n"));
+                    }
+                }
+            }catch (Exception ex){
+            }
+        }
+    }
+    public void choose(int row,int col,Button btn){
+        if(table[row][col] != -1){
+            MessageBox.show("That cell has been ticked","Please Tick Another");
+            return;
+        }
+        if(isPlaying && numberOfClickedButton % 2 == id ){
+//            btn.setGraphic(image);;
+//            table[row][col] = id;
+//            numberOfClickedButton++
+            try {
+                writer.writeObject(new Cell(row,col));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            MessageBox.show("Not your turn!","Please Wait");
+        }
+    }
+    public void updateCellPicture(int row,int col){
+        if(numberOfClickedButton%2 == 1) buttons[row][col].setText("X");
+        else buttons[row][col].setText("O");
+    }
+    public void setImage(){
+        if(id == 0) imageView.setImage(new Image("naruto.png"));
+        else imageView.setImage(new Image("sasuke.png"));
+    }
+    public int checkWin(){
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++) System.out.print(table[i][j] + " ");
+            System.out.println();
+        }
+        int r1 = checkNgang();
+        int r2 = checkDoc();
+        int r3 = checkCheo1();
+        int r4 = checkCheo2();
+
+
+        if(r1 == id || r2 == id  || r3 == id || r4 == id){
+            System.out.println("win");
+            return 1;
+        }
+
+        if(r1 == ((id+1)%2) || r2 == ((id+1)%2)  || r3 == ((id+1)%2) || r4 == ((id+1)%2)){
+            System.out.println("loose");
+            return 0;
+        }
+
+        return -1;
+    }
+    public int checkNgang(){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<=5;j++){
+                int v = table[i][j];
+                if( v!= -1 && v == table[i][j+1] && v == table[i][j+2] && v == table[i][j+3] && v == table[i][j+4]) return v;
+            }
+        }
+        return -1;
+    }
+    public int checkDoc(){
+        for(int j=0;j<9;j++){
+            for(int i=0;i<=5;i++){
+                int v = table[i][j];
+                if( v!= -1 && v == table[i+1][j] && v == table[i+2][j] && v == table[i+3][j] && v == table[i+4][j]) return v;
+            }
+        }
+        return -1;
+    }
+    public int checkCheo1(){
+        int u = 5;
+        while(u>=0){
+            int i = u, j = 0;
+            while(i + 4 < 10){
+                int x = table[i][j];
+                if(x != -1 && x == table[i+1][j+1] && x == table[i+2][j+2] && x == table[i+3][j+3] && x == table[i+4][j+4]) return x;
+                i++; j++;
+            }
+            u--;
+        }
+        int v = 5;
+        while(v>=0){
+            int i = 0, j = v;
+            while(j + 4 < 10){
+                int x = table[i][j];
+                if(x != -1 && x == table[i+1][j+1] && x == table[i+2][j+2] && x == table[i+3][j+3] && x == table[i+4][j+4]) return x;
+                i++; j++;
+            }
+            v--;
+        }
+        return -1;
+    }
+    public int checkCheo2(){
+        int u = 5;
+        while(u>=0){
+            int i = u, j = 9;
+            while(i + 4 < 10){
+                int x = table[i][j];
+                if(x != -1 && x == table[i+1][j-1] && x == table[i+2][j-2] && x == table[i+3][j-3] && x == table[i+4][j-4]) return x;
+                i++; j--;
+            }
+            u--;
+        }
+        int v = 4;
+        while(v < 10){
+            int i = 0, j = v;
+            while(j - 4  >= 0){
+                int x = table[i][j];
+                if(x != -1 && x == table[i+1][j-1] && x == table[i+2][j-2] && x == table[i+3][j-3] && x == table[i+4][j-4]) return x;
+                i++; j--;
+            }
+            v++;
+        }
+        return -1;
+    }
+    public void win(){
+        Platform.runLater(() ->{
+            numberOfClickedButton = (id+1)%2;
+            reset();
+            MessageBox.show("You won","Result");
+        });
+
+    }
+    public void loose(){
+        Platform.runLater(() ->{
+            numberOfClickedButton = id;
+            reset();
+            MessageBox.show("You loose","Result");
+        });
+    }
+    public void reset(){
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++) table[i][j] = -1;
+        }
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++) buttons[i][j].setText("");
+        }
+    }
+    public void sendMessage(){
+        try {
+            String textMessage = tfUsername.getText() + ": " + message.getText();
+            writer.writeObject(textMessage);
+            message.setText("");
+            message.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/sample/Server.java
+++ src/sample/Server.java
@@ -1,4 +1,68 @@
 package sample;
 
+        import java.io.*;
+        import java.net.ServerSocket;
+        import java.net.Socket;
+        import java.util.ArrayList;
+
 public class Server {
+    private ArrayList<ObjectOutputStream> clients;
+    public static void main(String[] args) throws IOException {
+        new Server().go();
+    }
+    public void go() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5008);
+        clients = new ArrayList<>();
+
+        while(clients.size() < 2){
+            Socket clientSocket = serverSocket.accept();
+            ObjectOutputStream writer = new ObjectOutputStream(clientSocket.getOutputStream());
+            clients.add(writer);
+            Thread t = new Thread(new ClientHandler(clientSocket));
+            t.start();
+            System.out.println("One client has connected!");
+            if(clients.size() == 2){
+                ObjectOutputStream out1 = clients.get(0);
+                out1.writeObject(0);
+                out1.flush();
+                ObjectOutputStream out2 = clients.get(1);
+                out2.writeObject(1);
+                out2.flush();
+                System.out.println("Sended");
+            }
+        }
+    }
+    private class ClientHandler implements Runnable {
+        private Socket socket;
+        private ObjectInputStream reader;
+
+        public ClientHandler(Socket socket) throws IOException {
+            this.socket = socket;
+            this.reader = new ObjectInputStream(socket.getInputStream());
+        }
+
+        @Override
+        public void run() {
+            Object object;
+            try {
+                while ((object = reader.readObject()) != null) {
+                    System.out.println(object.getClass());
+                    tellEverOne(object);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void tellEverOne(Object object){
+        for(ObjectOutputStream x  : clients){
+            try {
+                x.writeObject(object);
+                x.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
